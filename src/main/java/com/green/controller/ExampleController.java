7bc224fa02746d951ccf51b489493bc221f2a9c1
jpkno11 <jpkno66@naver.com>
2024-05-07 13:55:11 +0900

    열두번쨰 수업

--- src/main/java/com/green/controller/ExampleController.java
+++ src/main/java/com/green/controller/ExampleController.java
@@ -16,13 +16,14 @@ public class ExampleController {
    public String thymeleafExample(Model model) {
 		Person examplePerson = new Person();
 		examplePerson.setId(1L);
-		examplePerson.setName("홍길동");
-		examplePerson.setAge(11);
-		examplePerson.setHobbies(List.of("운동","독서"));
+		examplePerson.setName("손흥민");
+		examplePerson.setAge(31);
+		examplePerson.setHobbies(List.of("축구","게임"));
 		
-		 model.addAttribute("person, examplePerson");
-		 model.addAttribute("todat",LocalDate.now());
-		return "example"; // exampe.html 라는 뷰조회
+		 model.addAttribute("person", examplePerson);
+		 model.addAttribute("today", LocalDate.now()); 
+		// exampe.html 라는 뷰조회
+		 return "example";
 				
 	}
   @Setter
